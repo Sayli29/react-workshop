757162d7224fd987a404a535ca359d2f10591e34
Sayli29 <sayli.tawhare@gmail.com>
2023-08-12 10:24:02 +0530

    feat: added props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import Child from './Child'
 import './style.css';
 
 export default function App() {
+  const name = 'Prop'
   return (
     <div>
       <h1>React Workshop!</h1>
+      <h2>React Props</h2>
+      <Child  name={name}/>
     </div>
   );
 }
+
---
+++ src/Child.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Child(props){
+    return(
+        <>
+        <p>hello I am {props.name}!</p>
+        </>
+    );
+}
+
+export default Child;
