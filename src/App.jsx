4fde1df3d252400c5cea238b466d7c193c224c78
Sayli29 <sayli.tawhare@gmail.com>
2023-08-12 21:03:45 +0530

    refactor: added example of props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import * as React from 'react';
-import Child from './Child'
+import React from 'react';
+import Child from './Child';
 import './style.css';
 
 export default function App() {
-  const name = 'Prop'
+  const name = 'Prop';
+  const obj = { name: 'jerry', age: 10 };
+  const obj2 = { name: 'tom', age: 20 };
   return (
     <div>
       <h1>React Workshop!</h1>
       <h2>React Props</h2>
-      <Child  name={name}/>
+      <Child name={name} dataobj={obj} />
+      <ChildApp dataobj2={obj2} /> 
     </div>
   );
 }
 
+function ChildApp({ dataobj2 }) {
+  return (
+    <>
+      <p>childApp from App name here is {dataobj2.name}</p>
+      <p>childApp from App age here is {dataobj2.age}</p>
+    </>
+  );
+}
--- src/Child.jsx
+++ src/Child.jsx
@@ -4,6 +4,9 @@ function Child(props){
     return(
         <>
         <p>hello I am {props.name}!</p>
+        <p>hello I am object name {props.dataobj.name}</p>
+        <p>hello I am object name {props.dataobj.age}</p>
+
         </>
     );
 }
